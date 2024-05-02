caee1729f5381bcd70eb1dfb4eb7935386b4a000
Alberto Caballero <tesorodesabiduria28@gmail.com>
2024-05-02 13:14:14 -0500

    Add win checker feature, include player names in box selection, fix potential errors

--- Players.py
+++ Players.py
@@ -4,19 +4,20 @@ class Player:
         self.team = team
 
     def choose_box(self):
-        position = input(f"seleccione donde desea colocar su {self.team}: ")
+        position = input(f"{self.name}, seleccione donde desea colocar su {self.team}: ")
         return position
 
  
 def team_selection(player1, player2):
     teams = ["x", "0"]
     selected_teams = {}
-    t_player1 = input(f"Jugador 1, Seleccione su equipo{teams}: ")
+    t_player1 = input(f"{n_player1}, Seleccione su equipo{teams}: ")
     while t_player1 not in teams:
             t_player1 = input(f"Seleccione su equipo, que se encuetre aqui: {teams}")
     selected_teams[player1] = t_player1
     teams.remove(t_player1)
     selected_teams[player2] = teams[0] 
+    print(f"{n_player1} es {selected_teams[player1]} y {n_player2} es {selected_teams[player2]}" )
     return selected_teams
 
 n_player1 = input("Jugador 1, ingrese su nombre: ")
@@ -24,8 +25,6 @@ n_player2 = input("Jugador 2, ingrese su nombre: ")
 players_teams = team_selection(n_player1, n_player2)
 player1 = Player(n_player1,players_teams[n_player1])
 player2 = Player(n_player2,players_teams[n_player2])
-player1.choose_box()
-
 
 
 
--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,30 +1,112 @@
 import Players 
 class Tic_Tac_Toe:
-
+    
     def draw_board(self, player1, player2):
         spots = {1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9" }
         tracking_spots = {1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " " }
         
-        for i in range(1,10):
+        for i in range(1,11):
+
             board = (f"|{spots[1]}|{spots[2]}|{spots[3]}|\n"
-            f"|{spots[4]}|{spots[5]}|{spots[6]}|\n"
-            f"|{spots[7]}|{spots[8]}|{spots[9]}|\n")
+                    f"|{spots[4]}|{spots[5]}|{spots[6]}|\n"
+                    f"|{spots[7]}|{spots[8]}|{spots[9]}|\n")
             print(board)
-            if i % 2 == 0:
-                box = int(player1.choose_box())
+
+            results = self.winner_cheker(spots, player1, player2)
+            if results == True:
+                break
+
+            if i == 10:
+                print("El juego a quedado en un empate")
+                print(board)
+
+            elif i % 2 == 0:
+
+                try:
+                    box = int(player1.choose_box())
+                except ValueError:
+                    print("upss, ese valor no se presenta en la tabla")
+                    box = int(player2.choose_box())
+
+                while box not in spots.keys():
+                    print("Te fuiste, ese valor no esta en la tabla")
+                    box = int(player1.choose_box())
+
+
                 while tracking_spots[box] != " ":
                     print("This spot is already taken, choose a empty one")
                     box = int(player1.choose_box())
+                    
                 spots[box] = player1.team
                 tracking_spots[box] = player1.team
+
             else:
-                box = int(player2.choose_box())
+
+                try:
+                    box = int(player2.choose_box())
+                except ValueError:
+                    print("upss, ese valor no se presenta en la tabla")
+                    box = int(player2.choose_box())
+
+                while box not in spots.keys():
+                    print("Te fuiste, ese valor no esta en la tabla")
+                    box = int(player2.choose_box())
+
                 while tracking_spots[box] != " ":
                     print("This spot is already taken, choose a empty one")
                     box = int(player2.choose_box())
+
                 spots[box] = player2.team
                 tracking_spots[box] = player2.team
-                print(board)
+
+                
+
+
+
+    def winner_cheker(self, board, player1, player2):
+        
+        for i in range(1, 8, 3):
+             
+            if board[i] == board[i+1] == board[i+2]:
+                if player1.team == board[1]:
+                    print(f"{player1.name} es el ganador!!!")
+                    return True
+                
+                elif player2.team == board[1]:
+                    print(f"{player2.name} es el ganador!!!!")
+                    return True
+                
+                else:
+                    return False
+                 
+        for i in range(1, 4):
+
+             if board[i] == board[i+3] == board[i+6]:
+             
+                if player1.team == board[1]:
+                    print(f"{player1.name} es el ganador!!!")
+                    return True
+                
+                elif player2.team == board[1]:
+                    print(f"{player2.name} es el ganador!!!")
+                    return True 
+                
+                else: 
+                    return False
+                
+        
+        if board[1] == board[5] == board[9] or board[3] == board[5] == board[7]:
+
+            if player1.team == board[1]:
+                print(f"{player1.name} es el ganador!!!")
+                return True
+
+            elif player2.team == board[1]:
+                print(f"{player2.name} es el ganador!!!")
+                return True
+            
+            else:
+                return False
 
 
 game_1 = Tic_Tac_Toe()
